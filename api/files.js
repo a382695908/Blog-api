34593a8a9f752414dcfdd0b43821c340b39c74ea
yyj <yyjazsf@live.cn>
2016-03-13 22:50:15 +0800

    文件上传,MD5_fileSize.extName

--- api/files.js
+++ api/files.js
@@ -10,22 +10,24 @@
  */
 
 const express = require('express');
-const md5 = require('crypto').createHash('md5');//文件秒传  md5+文件字节数(双验证)
+const crypto = require('crypto');//文件秒传  md5+文件字节数(双验证)
+const path = require('path');
 const fs = require('fs');
 
 let router = express.Router();
 
-let file_save = __dirname +'/../public/upload';//+  /public/upload/type/id.extName
+let file_save = path.join(__dirname, '/../public/upload');//+  /public/upload/type/id.extName
 
 /**
  * /api/files
  * get 获取文件列表(有必要?)
  * post 上传文件(返回id,图片类的返回url)
+ * todo 保存原始文件名,按需要显示到客户端
  */
 router
     .route('/')
     .get(function (req, res, next) {
-      fs.readdir( file_save, function(err, files) {
+      fs.readdir(file_save, function (err, files) {
 
         if (err) {
 
@@ -35,29 +37,45 @@ router
       });
     })
     .post(function (req, res, next) {
-
+      let md5 = crypto.createHash('md5');
       let fstream,
-          md5_code;
+          tempUrl;
 
       req.pipe(req.busboy);
       //
       req.busboy.on('file', function (fieldname, file, filename, encoding, mimetype) {
 
-        fstream = fs.createWriteStream(file_save + '/img/' + filename);//放临时文件夹
+        tempUrl = path.join(file_save, '/temp/', filename);
+
+        fstream = fs.createWriteStream(tempUrl);//放临时文件夹
 
         file.pipe(fstream);
 
-        console.log(fstream);
-
-        //fstream.on('data',()=>{
-        //  md5.update();
-        //});
-
         fstream.on('close', function () {
-          //todo 把文件从临时文件夹,移到最终,
-          //md5_code=md5.digest('hex');
-          console.log(`Upload Finished of ${md5_code}`);
-          res.json([fieldname, file, filename, encoding, mimetype]);
+          //上传完毕,读取MD5,并重命名MD5_FileSize.extName
+
+          let s = fs.ReadStream(tempUrl);
+          s.on('data', function (d) {
+            md5.update(d);
+          });
+          s.on('end', function () {
+            let d = md5.digest('hex');
+            let fileName;
+            fs.stat(tempUrl, (err, stats)=> {
+              fileName = d + '_' + stats.size + path.extname(filename);
+              fs.rename(tempUrl, path.join(file_save, '/img/', fileName), (err)=> {
+                if (err) {
+                  next(err);
+                  return;
+                }
+                console.log(s);
+                res.send('上传成功:' + d);
+
+              });
+
+            });
+          });
+
         });
       });
 
@@ -67,6 +85,23 @@ router
 
     });
 
+/**
+ * /api/files/exists/:fileName
+ * 检查文件一致性
+ * @param fileName md5_size.extName
+ * get
+ */
+router
+    .route('/exists/:fileName')
+    .get((req, res, next)=> {
+      let fileName = req.params.md5Size;
+      fs.exists(path.join(file_save, '/img/', fileName), (exists)=> {
+        res.json({
+          success:true,
+          result: exists
+        });
+      });
+    });
 /**
  * /api/files/:id
  * get 下载文件
@@ -84,6 +119,19 @@ router
     });
 
 //错误统一显示
+router.use((err, req, res, next)=> {
+
+  if (err) {
+    res.json({
+      success: false,
+      msg: err.message
+    });
+  }
+  else {
+    next();
+  }
+
+});
 
 
 module.exports = router;
